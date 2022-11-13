938453dca9f4f45333f4044fa62a243245e68226
Jabame <justinetaglucop@gmail.com>
2022-11-13 22:48:48 +0800

    Changed the Query syntax

--- payments_val.php
+++ payments_val.php
@@ -19,8 +19,8 @@ if (isset($_POST['payment_button_val'])) {
 
     $credit_card_number = $_POST['credit_card_number'];
     $credit_card_expr_date = date('Y-m-d', strtotime($_POST['date_expr']));
-   
-    $last_id = "SELECT LAST_INSERT_ID()";
+
+
 
     $query = "UPDATE flight_users
                 SET
@@ -29,7 +29,9 @@ if (isset($_POST['payment_button_val'])) {
                 passenger_names = '$passenger_names', credit_card_number = '$credit_card_number',
                 credit_card_expr_date = '$credit_card_expr_date'
                 WHERE
-                users_id = $loginId
+                users_id = '$loginId'
+                order by id DESC limit 1
+           
                 ";
     //  $query = "INSERT INTO flight_users (id,firstname, lastname, street_address, city_zip, passenger_names, credit_card_number, credit_card_expr_date) 
     //  VALUES ('$last_id', '$fName','$lName', '$street_address', '$city_zip', '$passenger_names','$credit_card_number', '$credit_card_expr_date') ";
@@ -42,6 +44,7 @@ if (isset($_POST['payment_button_val'])) {
         header("Location: invoice.php");
     } else {
         echo mysqli_error($conn);
+  
     }
 
 
